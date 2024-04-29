cab4e240ccedae54ef752c0791d891470ae1025b
navmodder <singh.navjot@live.com>
2024-04-29 11:06:45 -0400

    r

--- Loader/Themes/Run.bat
+++ Loader/Themes/Run.bat
@@ -3,7 +3,8 @@ setlocal enabledelayedexpansion
 set json=
 set count=1
 for /f "tokens=*" %%i in ('dir /b /ad /on') do (
-    set json=!json!"!count!": "%%i",   
+    if defined json set json=!json!,
+    set json=!json!"!count!": "%%i"
     set /a count+=1
 )
-(echo { & echo %json:~0,-1% & echo }) > Themes.json
+(echo { & echo %json% & echo }) > Themes.json
