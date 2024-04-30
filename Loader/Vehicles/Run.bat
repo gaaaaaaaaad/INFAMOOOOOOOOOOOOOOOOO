fe60daf212cace0b5774e9fe4b0b5c400f426895
navmodder <singh.navjot@live.com>
2024-04-30 18:55:50 -0400

    re

---
+++ Loader/Vehicles/Run.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal enabledelayedexpansion
+set json=
+set count=1
+for /f "tokens=*" %%i in ('dir /b /ad /on') do (
+    if defined json set json=!json!,
+    set json=!json!"!count!": "%%i"
+    set /a count+=1
+)
+(echo { & echo %json% & echo }) > Vehicles.json
